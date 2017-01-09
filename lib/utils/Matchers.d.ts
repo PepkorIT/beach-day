0858aa83facaa6562332a153b0144601f18ca026
Chris Beech <chris@leftandright.co.za>
2017-01-09 12:14:05 +0200

    CDMOB-453 - Remove d.ts compilation for beach-day source files

--- lib/utils/Matchers.d.ts
+++
@@ -1,3 +0,0 @@
-export declare var Matchers: {
-    registerMatchers: () => void;
-};
--- lib/utils/ObjectUtils.d.ts
+++
@@ -1,4 +0,0 @@
-export default class ObjectUtils {
-    static getProp(source: Object | Array<any>, propertyAccessor: string, convertUndefinedToNull?: boolean): any;
-    static setProp(target: any, propertyAccessor: string, value: any): any;
-}
