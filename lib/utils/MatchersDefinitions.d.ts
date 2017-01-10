71144fe40314b9226d1edf0c26b85aa9fcc399d0
Jonathan Beech <jon@leftandright.co.za>
2017-01-10 12:09:51 +0200

    Dropped types moved declaration

--- lib/utils/MatchersDefinitions.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace jasmine {
-    export interface Matchers {
-        toBePassing(): boolean;
-        throwExpectError(message:string): boolean;
-        throwImplementationError(message:string): boolean;
-        statusCodeToBe(statusCode:number): boolean;
-    }
-}
