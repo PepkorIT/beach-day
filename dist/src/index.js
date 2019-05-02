b0f32cc2bf73a73a1831d1f583ce0c5862f45d00
Jonathan Beech <jon@leftandright.co.za>
2019-05-02 10:46:55 +0200

    regenerated js

--- dist/src/index.js
+++ dist/src/index.js
@@ -4,14 +4,16 @@ function __export(m) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 var matchers_1 = require("./utils/matchers");
+__export(require("./network/call-config"));
+__export(require("./network/extending-object"));
+__export(require("./network/request-runner"));
 __export(require("./reporter/beach-day-reporter"));
 __export(require("./utils/jasmine-async-env"));
+__export(require("./utils/matcher-utils"));
+__export(require("./utils/object-utils"));
 __export(require("./utils/random-utils"));
 __export(require("./utils/swagger-utils"));
 __export(require("./utils/test-utils"));
-__export(require("./utils/object-utils"));
-__export(require("./network/call-config"));
-__export(require("./network/request-runner"));
 // Register initially, this will help register matchers
 // if the implementer decides to use this framework without the reporter
 matchers_1.Matchers.registerMatchers();
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import {Matchers} from './utils/matchers';
 
+export * from './network/call-config';
+export * from './network/extending-object';
+export * from './network/i-request-response';
+export * from './network/request-runner';
 export * from './reporter/beach-day-reporter';
 export * from './utils/jasmine-async-env';
+export * from './utils/matcher-utils';
+export * from './utils/object-utils';
 export * from './utils/random-utils';
 export * from './utils/swagger-utils';
 export * from './utils/test-utils';
-export * from './utils/object-utils';
-export * from './network/call-config';
-export * from './network/i-request-response';
-export * from './network/request-runner';
 
 // Register initially, this will help register matchers
 // if the implementer decides to use this framework without the reporter
